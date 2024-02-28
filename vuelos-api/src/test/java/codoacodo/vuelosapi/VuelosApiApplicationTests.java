6f19ec4e19555d9f97429e20adc6308ec2c82c09
Veronica <dv.veronica2075@gmail.com>
2024-02-28 18:27:37 -0300

    juego con rojo

---
+++ vuelos-api/src/test/java/codoacodo/vuelosapi/VuelosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package codoacodo.vuelosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuelosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
