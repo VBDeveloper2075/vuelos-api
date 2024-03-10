84ebf02ba78b2b09f48b5003190441a6a9a7f02d
Veronica <dv.veronica2075@gmail.com>
2024-03-09 23:20:55 -0300

    crud terminado

--- vuelos-api/src/main/java/codoacodo/vuelosapi/controller/VueloController.java
+++ vuelos-api/src/main/java/codoacodo/vuelosapi/controller/VueloController.java
@@ -1,83 +1,43 @@
-package codoacodo.vuelosapi.controller;
+package codoacodo.vuelosapi;
 
 import codoacodo.vuelosapi.model.Vuelo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/vuelos")
+@RequestMapping("/api/vuelos")
 public class VueloController {
 
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+    @Autowired
+    private service vueloService;
 
-    private List<Vuelo> vuelos = new ArrayList<>();
-
-    //    no funcionaba porque la fecha estaba en formato string, entronces lo parseo a formato LocalDateTime
-    Vuelo vuelo1 = new Vuelo(1L, "Buenos Aires", "Madrid", LocalDateTime.parse("2021-10-10T10:00:00", formatter), LocalDateTime.parse("2021-10-10T20:00:00", formatter), 100000, "diario");
-    Vuelo vuelo2 = new Vuelo(2L, "Buenos Aires", "Miami", LocalDateTime.parse("2021-10-10T10:00:00", formatter), LocalDateTime.parse("2021-10-10T20:00:00", formatter), 120000, "diario");
-    Vuelo vuelo3 = new Vuelo(3L, "Buenos Aires", "Roma", LocalDateTime.parse("2021-10-10T10:00:00", formatter), LocalDateTime.parse("2021-10-10T20:00:00", formatter), 110000, "diario");
-    Vuelo vuelo4 = new Vuelo(4L, "Buenos Aires", "Nueva York", LocalDateTime.parse("2021-10-10T10:00:00", formatter), LocalDateTime.parse("2021-10-10T20:00:00", formatter), 130000, "diario");
-    Vuelo vuelo5 = new Vuelo(5L, "Buenos Aires", "Londres", LocalDateTime.parse("2021-10-10T10:00:00", formatter), LocalDateTime.parse("2021-10-10T20:00:00", formatter), 140000, "diario");
-
-
-    @GetMapping("/")
-    public String hola() {
-        return "<h1> Hola Mundo </h1>";
+    @PostMapping
+    public Vuelo crearVuelo(@RequestBody Vuelo vuelo) {
+        return vueloService.crearVuelo(vuelo);
     }
 
-    @GetMapping("/listar")
-    public Vuelo[] listarVuelos() {
-        Vuelo[] vuelos = {vuelo1, vuelo2, vuelo3, vuelo4, vuelo5};
-        return vuelos;
+    @GetMapping
+    public List<Vuelo> listarVuelos() {
+        return (List<Vuelo>) vueloService.listarVuelos();
     }
 
-    //en localhost:8080/adios => veo el mensaje "Adios Mundo"
-    @GetMapping("/adios")
-    public String adios() {
-        return "Adios Mundo";
+    @GetMapping("/{id}")
+    public Vuelo obtenerVueloPorId(@PathVariable Long id) {
+        return vueloService.devolverVuelo(id);
     }
 
-    @GetMapping("/vuelo")
-    public String mostrarVuelo() {
-        Vuelo vuelo = new Vuelo(1L, "Buenos Aires", "Roma", LocalDateTime.now(), LocalDateTime.now().plusHours(13), 1000.0, "Diario");
-        return "Vuelo: " + "\nID: " + vuelo.getId() + "\nOrigen: " + vuelo.getOrigen() + "\nDestino: " + vuelo.getDestino() + "\nFecha y hora de salida: " + vuelo.getFechaHoraSalida() + "\nFecha y hora de llegada: " + vuelo.getFechaHoraLlegada() + "\nPrecio: " + vuelo.getPrecioEnPesos() + "\nFrecuencia: " + vuelo.getFrecuencia();
+    @PatchMapping("/{id}")
+    public Vuelo actualizarVuelo(@PathVariable Long id, @RequestBody Vuelo vuelo) {
+        vuelo.setId(id);
+        return vueloService.actualizarVuelo(vuelo);
     }
 
-    @PostMapping("/crear")
-    public Vuelo crearVuelo(@RequestBody Vuelo nuevoVuelo) {
-        vuelos.add(nuevoVuelo);
-        return nuevoVuelo;
-    }
-
-    @PutMapping("/actualizar/{id}")
-    public Vuelo actualizarVuelo(@PathVariable Long id, @RequestBody Vuelo vueloActualizado) {
-        for (int i = 0; i < vuelos.size(); i++) {  //metodo para obtener el largo de una coleccion
-            if (vuelos.get(i).getId().equals(id)) {
-                vuelos.set(i, vueloActualizado);
-                return vueloActualizado;
-            }
-        }
-        return null;
-    }
-
-    @DeleteMapping("/borrar/{id}")
-    public String borrarVuelo(@PathVariable Long id) {
-        for (int i = 0; i < vuelos.size(); i++) {
-            if (vuelos.get(i).getId().equals(id)) {
-                vuelos.remove(i);
-                return "Vuelo con id " + id + " borrado";
-            }
-        }
-        // mensaje si no se encontró el vuelo
-        return "Vuelo con id " + id + " no encontrado";
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> borrarVuelo(@PathVariable Long id) {
+        vueloService.borrarVuelo(id);
+        return ResponseEntity.ok().build();
     }
 }
-
-
-
-
-
---
+++ vuelos-api/src/main/java/codoacodo/vuelosapi/service/VueloServiceImpl.java
@@ -0,0 +1,2 @@
+package codoacodo.vuelosapi.service;public class VueloServiceImpl {
+}
