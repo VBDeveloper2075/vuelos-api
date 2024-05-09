e44981eb03e40f818777822ede99a950ee164ceb
Veronica <dv.veronica2075@gmail.com>
2024-05-09 19:49:10 -0300

    requestmapping

--- vuelos-api/src/main/java/codoacodo/vuelosapi/controller/HolaMundoControler.java
+++ vuelos-api/src/main/java/codoacodo/vuelosapi/controller/HolaMundoControler.java
@@ -1,6 +1,7 @@
 package codoacodo.vuelosapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -10,5 +11,10 @@ public class HolaMundoControler {
     public String holaMundo(@PathVariable String nombreUsuario) {
         System.out.println("Ejecutando el método holaMundo()");
         return "Hola Mundo Spring Boot!. Bienvenido/a " + nombreUsuario + " a la API de Vuelos";
+    }
+
+    @RequestMapping(value="/index-req-mapping")
+    public String indexRequestMapping() {
+        return "Hola Mundo Spring Boot!. Bienvenido/a a la API de Vuelos";
     }
 }
